936c325eb450ff4eee9f626042851be80ae26d11
James Jacob <jayj1121@gmail.com>
2021-09-21 00:14:22 -0400

    Added dropdown color switching to create menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
   public GameObject main;
   public GameObject create;
+
+  public Dropdown player1Type;
+  public Dropdown player1Color;
+  public Dropdown player2Type;
+  public Dropdown player2Color;
+
+  private Color lightRed;
+  private Color red;
+  private Color lightBlue;
+  private Color blue;
 
   public GameObject tile;
   public int side = 100;
@@ -43,11 +54,48 @@ public class MainMenuManager : MonoBehaviour
     }
   }
 
+  public void ChangeDropdownColor(string text, Dropdown drop)
+  {
+    ColorBlock block = drop.colors;
+
+    if (text.Equals("Red"))
+    {
+      block.normalColor = lightRed;
+      block.selectedColor = lightRed;
+      block.highlightedColor = red;
+
+      drop.colors = block;
+    }
+    else if (text.Equals("Blue"))
+    {
+      block.normalColor = lightBlue;
+      block.selectedColor = lightBlue;
+      block.highlightedColor = blue;
+
+      drop.colors = block;
+    }
+  }
+
   // Start is called before the first frame update
   void Start()
   {
     main.SetActive(true);
     create.SetActive(false);
+
+    lightRed = new Color(0.9607844f, 0.4117647f, 0.4117647f, 1.0f);
+    red = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    lightBlue = new Color(0.3960785f, 0.627451f, 0.9215687f, 1.0f);
+    blue = new Color(0.0f, 0.3058824f, 1.0f, 1.0f);
+    
+    player1Color.onValueChanged.AddListener(delegate {
+      ChangeDropdownColor(player1Color.captionText.text, player1Type);
+      ChangeDropdownColor(player1Color.captionText.text, player1Color);
+    });
+
+    player2Color.onValueChanged.AddListener(delegate {
+      ChangeDropdownColor(player2Color.captionText.text, player2Type);
+      ChangeDropdownColor(player2Color.captionText.text, player2Color);
+    });
 
     Time.timeScale = 1;
 
